fd967b595c3468b62c424772471c73a858c3a4cf
Rithvika-Keerthi <rithvika.keerthi@cognizant.com>
2025-04-03 17:15:10 +0530

    Add files via upload

---
+++ SHMClassLibrary-main/Data/BookingDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SHMClassLibrary
+{
+    public class BookingDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= localhost;Initial Catalog=BookingDetails2; Integrated Security =true; TrustServerCertificate=true");
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+//using HotlelManagementClass;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace HotelManagementClass
+//{
+//    public class BookingPaymentContext : DbContext
+//    {
+//        public DbSet<Hotel> Hotels { get; set; }
+//        public DbSet<Room> Rooms { get; set; }
+//        public DbSet<Booking> Bookings { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            optionsBuilder.UseSqlServer("Data Source= localhost;Initial Catalog=BookingDetails2; Integrated Security =true; TrustServerCertificate=true");
+//        }
+//    }
+//}
+
+
+
