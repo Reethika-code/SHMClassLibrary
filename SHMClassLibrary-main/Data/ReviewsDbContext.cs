3c0fe5c152901a25e38f4e0898d4b2d611b231b6
Harshini2508-code <kanapuram.harshini@cognizant.com>
2025-04-03 16:12:58 +0530

    Add files via upload

---
+++ SHMClassLibrary-main/Data/ReviewsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SHMClassLibrary.Entities;
+
+namespace SHMClassLibrary.Data
+{
+    public class ReviewsDbContext : DbContext
+    {
+        public DbSet<Review> Reviews { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = LTIN592795; Initial Catalog = SmartHotelDb; Integrated Security = true; TrustServerCertificate = true; ");
+        }
+
+    }
+}
