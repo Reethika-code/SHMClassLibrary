055fa4cd5f69a329f584dec0349bd5556c60a6b7
Harshini2508-code <kanapuram.harshini@cognizant.com>
2025-04-03 16:13:42 +0530

    Add files via upload

---
+++ SHMClassLibrary-main/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace SHMClassLibrary.Entities
+{
+    public class Review
+    {
+       // ReviewID, UserID, HotelID, Rating, Comment, Timestamp
+        public int ReviewID { get; set; }
+        public int UserID { get; set; }
+        public int HotelID { get; set; }
+        public double Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool IsFlagged { get; set; }
+    }
+}
