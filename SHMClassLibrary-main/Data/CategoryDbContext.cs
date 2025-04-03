d1b7f07b17ed1774e8b7ab236ed93fac46387d4f
Reethika-code <reethika.gajulapati@cognizant.com>
2025-04-03 18:46:15 +0530

    Add files via upload

---
+++ SHMClassLibrary-main/Data/CategoryDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using SHMClassLibrary.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHMClassLibrary
+{
+    public class CategoryDbContext : DbContext
+    {
+        public DbSet<CategoryModel> Categories { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=LTIN589782;User=sa;Password=root;Initial Catalog=SmartHotelDb;Integrated Security=True;TrustServerCertificate=true");
+            }
+        }
+        
+    }
+}
+
