c4ce693e15969b297f7270dc098d383f7d51f870
Vanitha27-code <Nalimela.Vanitha@cognizant.com>
2025-04-03 17:59:49 +0530

    Add files via upload

---
+++ SHMClassLibrary-main/Entities/Room_New.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHMClassLibrary
+{
+    
+        public class Room_New
+        {
+            public int RoomID { get; set; }
+            public int HotelID { get; set; }
+            public string Type { get; set; }
+            public float Price { get; set; }
+            public string Availability { get; set; }
+            public string Features { get; set; }
+            public bool IsActive { get; set; }
+        }
+    
+}
